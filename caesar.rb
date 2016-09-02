27af84908099b7922064aa78f20a7e0dc9ea100b
Zach Beaird <zbbeaird1989@gmail.com>
2016-09-01 22:17:43 -0500

    test merge
    exit
    Q
    q
    git log
    exit

--- caesar.rb
+++ caesar.rb
@@ -1,33 +1,28 @@
 def caesar_cipher(string, shift)
 
-	#Seperate words and letters
-	words = string.split(" ").collect { |word| word.split("")}
 	alphabet_values = {}
-	new_words = []
+	new_words = ""
 
 	#Gives all letters a numerical value
 	("a".."z").each_with_index { |letter, index| alphabet_values[letter.to_sym] = index + 1}
 
-
 	#Creates new word
-	words.each do |word|
-		new_word = []
-		word.each do |letter|
+	string.each_char do |letter|
+		unless letter == " "
 			new_value = alphabet_values[letter.to_sym] + shift
 			#Wraps alphabet values
 			if new_value > 26
-				new_value = new_value - 26
+				new_value -= 26
 			end
 			alphabet_values.each do |key, value|
-				new_word.push(key.to_s) if value == new_value
+				new_words << key.to_s if value == new_value
 			end
+		else
+			new_words << letter #add whitespace to string
 		end
-		new_words.push(new_word)
 	end
 
-	output = new_words.collect { |word| word.join }
-
-	return output.join(" ")
+	new_words
 end
 
 puts caesar_cipher("hello world", 5)
