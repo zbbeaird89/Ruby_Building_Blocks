2dc6a5b31416c9aa1c2671995564cd4e69af5ea7
Zach Beaird <zbbeaird1989@gmail.com>
2016-09-01 20:19:07 -0500

    tested regex on string

--- caesar.rb
+++ caesar.rb
@@ -1,21 +1,11 @@
 def caesar_cipher(string, shift)
 
 	#Seperate words and letters
-	words = string.split(" ").collect { |word| word.split("")}
+	words = []
+	words.push(string.gsub(/(.+\b)/))
 	alphabet_values = {}
 
 	("a".."z").each_with_index { |letter, index| alphabet_values[letter.to_sym] = index + 1}
-
-=begin
-	letters.each do |letter|
-
-		if alphabet_values[letter.to_sym]
-			puts "#{alphabet_values[letter.to_sym]}"
-		end
-
-	end
-
-=end
 
 	words.each do |word|
 		word.each do |letter|
@@ -25,8 +15,7 @@ def caesar_cipher(string, shift)
 		end
 	end
 
-	
-
+	puts "#{words}" 
 
 end
 
