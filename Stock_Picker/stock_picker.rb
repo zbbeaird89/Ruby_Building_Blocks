6b3ba1606d127961893a6c4c65bb1dae943a7787
Zach Beaird <zbbeaird1989@gmail.com>
2016-09-05 19:52:41 -0500

    Solved problem

--- Stock_Picker/stock_picker.rb
+++ Stock_Picker/stock_picker.rb
@@ -1,23 +1,29 @@
 def stock_picker(array)
-	
-	i     = 0
-	j     = i
+
 	profits = []
+	best_days = nil
+	i = 0
+	j = i
 	
+	#Compares all possible days for buying and selling stock and returns the 2 best	
 	while i < array.length do 
 		while j < array.length do 
-			profits.push(array[j] - array[i])
+			buy, sell, diff  = array[i], array[j], array[j] - array[i]
+			profits.push(diff)
+			if diff == profits.max
+				best_days = [array.index(buy), array.index(sell)]
+			end
 			j += 1
 		end
 		i += 1
 		j = i
 	end
-	puts "The differences for #{array[i]} are: #{profits}."
-	puts "The max of the differences is: #{profits.max}"
+
+	best_days
 end
 
 
 #stock_picker([17,3,6,9,15,8,6,1,10])
 #[1, 4]
 
-stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
