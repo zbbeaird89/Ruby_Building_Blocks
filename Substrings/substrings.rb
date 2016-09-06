12c396d2e3f447ea75eb1cd4305ec209e5be3940
Zach Beaird <zbbeaird1989@gmail.com>
2016-09-05 22:00:09 -0500

    Solved problem

---
+++ Substrings/substrings.rb
@@ -0,0 +1,38 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, array)
+
+	output = {}
+	i      = 0
+	j      = 0
+
+	while i < string.length do
+		while j < string.length do
+			range = string[i..j]
+			if array.include?(range)
+				if output[range].is_a?(Fixnum)
+					output[range] += 1
+				else
+				   output[range] = 1
+				end
+			end
+			j += 1
+		end
+		i += 1
+		j = i
+	end
+
+	puts output
+end
+
+#substrings("below", dictionary)
+#=> {"below"=>1, "low"=>1}
+
+substrings("going going", dictionary)
+
+
+
+# Method will take a word as a first argument and an array of valid substrings as the second.
+
+# The method will return a hash that lists all of the substrings that were found in the word
+# that was passed and give a number of how many times each substring was found.
